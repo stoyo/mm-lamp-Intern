c4b66155989a0ede8d4e8443dc9f8e356b9643ca
Stoyan Genchev <genchev_stoyan@abv.bg>
2016-08-08 18:29:42 +0300

    added 2D SolarSystem

---
+++ SolarSystem/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Solar System</title>
+        <link rel="stylesheet" type="text/css" href="./styles/style.css">
+    </head>
+    <body>
+        <div class="orbit pluto">
+            <div class="planet pl-pluto"></div>
+        </div>
+        <div class="orbit neptune">
+            <div class="planet pl-neptune"></div>
+        </div>
+        <div class="orbit uranus">
+            <div class="planet pl-uranus"></div>
+        </div>
+        <div class="orbit saturn">
+            <div class="planet pl-saturn"></div>
+        </div>
+        <div class="orbit jupiter">
+            <div class="planet pl-jupiter"></div>
+        </div>
+        <div class="orbit mars">
+            <div class="planet pl-mars"></div>
+        </div>
+        <div class="orbit earth">
+            <div class="planet pl-earth">
+                <div class="earth-orbit"></div>
+            </div>
+        </div>
+        <div class="orbit venus">
+            <div class="planet pl-venus"></div>
+        </div>
+        <div class="orbit mercury">
+            <div class="planet pl-mercury"></div>
+        </div>
+        <div class="sun"></div>
+    </body>
+</html>
